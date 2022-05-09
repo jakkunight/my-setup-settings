4fd9fbe5cf39206b6e69e15fa08e3d8ec43d5b15
Jakku Night <j97k107u45k117@gmail.com>
2022-05-09 12:00:43 -0400

    Added support for Termux storage and access to 'Downloads' dir.

--- install.sh
+++ install.sh
@@ -1,33 +1,34 @@
 # Script to install all the necesary tools for development
-echo "Read the sript file after running. It will help you."
+echo "    Read the sript file after running. It will help you."
 
 # Setups the nodejs/expo/git basic enviroment:
-echo "Updating your system..." 
+echo "    Updating your system..."
 apt update -y
-echo "Insatalling git and nodejs..."
+echo "    Insatalling git and nodejs..."
 apt install git nodejs-lts -y
 # Upgrades npm:
-echo "Installing npm..."
+echo "    Installing npm..."
 curl https://npmjs.org/install.sh | sh
 # Installs Expo and yarn
-echo "Installing Expo and yarn..."
+echo "    Installing Expo and yarn..."
 npm i -g expo-cli eas-cli nodemon yarn -y
 
 # Installs tmux (optional):
-echo "Installing tmux..."
+echo "    Installing tmux..."
 apt install tmux -y
 
 # Installs vim:
-echo "Installing vim..."
+echo "    Installing vim..."
 apt install vim -y
 
 # Clones the config files into your root folder:
-echo "Replacing config files for vim, bash and nano..."
+echo "    Replacing config files for vim, bash and nano..."
 cp ./.bashrc ~
 cp ./.vimrc ~
 cp ./.nanorc ~
 
 # Clones the syntax highlighting config files from @scopatz
+echo "    Cloning syntax highlighting config files and creating nano backup dir..."
 cd ~
 mkdir .nano
 cd .nano
@@ -35,8 +36,12 @@ mkdir backups
 git clone https://github.com/scopatz/nanorc
 
 # Installs mysql:
-echo "Installing mariadb..."
+echo "    Installing mariadb..."
 apt install mariadb -y
 
+# Mounts storage and access to downloads dir:
+echo "    Mounting Termux storage system..."
+termux-setup-storage
+
 # Exit:
-echo "See the Termux Wiki for more info."
+echo "    See the Termux Wiki for more info."
