50ef12b88e348042b42a213a315c650bf3a3d1ee
Jakku Night <R101g117l117s@gmail.com>
2022-05-15 12:21:41 -0400

    Added root support.

--- install.sh
+++ install.sh
@@ -3,9 +3,9 @@ echo "    Read the sript file after running. It will help you."
 
 # Setups the nodejs/expo/git basic enviroment:
 echo "    Updating your system..."
-pkg update -y
+apt update -y
 echo "    Insatalling git, curl, wget, sqlite and nodejs..."
-pkg install git curl wget sqlite nodejs-lts -y
+apt install git curl wget sqlite nodejs-lts -y
 # Upgrades npm:
 echo "    Installing npm..."
 curl -qL https://www.npmjs.com/install.sh | sh
@@ -15,7 +15,7 @@ npm i -g expo-cli eas-cli nodemon yarn -y
 
 # Installs tmux (optional):
 echo "    Installing tmux..."
-pkg install tmux -y
+apt install tmux -y
 
 s into your root folder:
 echo "    Replacing config files for bash and nano..."
@@ -41,7 +41,7 @@ fi
 
 # Installs mysql:
 echo "    Installing mariadb..."
-pkg install mariadb -y
+apt install mariadb -y
 
 # Mounts storage and access to downloads dir:
 echo "    Mounting Termux storage system..."
@@ -49,11 +49,11 @@ termux-setup-storage
 
 # Installs screenfetch for tunning the enviroment:
 echo "    Installing screenfetch..."
-pkg install screenfetch -y
+apt install screenfetch -y
 
 # Installs htop for control how much RAM are we consuming:
 echo "    Installing htop..."
-pkg install htop -y
+apt install htop -y
 
 # WARNING: ROOT ZONE!!!
 # Asks for root and tries to setup your enviroment according to this:
