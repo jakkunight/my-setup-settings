d1f9cdff1d2d39ef0975308848b361dbecbc3815
Jakku Night <R101g117l117s@gmail.com>
2022-05-16 12:15:41 -0400

    Added Kali Nethunter support.

--- install.sh
+++ install.sh
@@ -1,59 +1,7 @@
 # Script to install all the necesary tools for development
 echo "    Read the sript file after running. It will help you."
-
-# Setups the nodejs/expo/git basic enviroment:
 echo "    Updating your system..."
 pkg update -y
-echo "    Insatalling git, curl, wget, sqlite and nodejs..."
-pkg install git curl wget sqlite nodejs-lts -y
-# Upgrades npm:
-echo "    Installing npm..."
-curl -qL https://www.npmjs.com/install.sh | sh
-# Installs Expo and yarn
-echo "    Installing Expo and yarn..."
-npm i -g expo-cli eas-cli nodemon yarn -y
-
-# Installs tmux (optional):
-echo "    Installing tmux..."
-pkg install tmux -y
-
-s into your root folder:
-echo "    Replacing config files for bash and nano..."
-cp -f ./.bashrc ~
-cp -f ./.nanorc ~
-
-# Clones the syntax highlighting config files from @scopatz
-echo "    Cloning syntax highlighting config files and creating nano backup dir..."
-cd ~
-if [ ! -d .nano ]
-then
-	mkdir .nano
-fi
-cd .nano
-if [ ! -d backups ]
-then
-	mkdir backups
-fi
-if [ ! -d nanorc ]
-then
-	git clone https://github.com/scopatz/nanorc
-fi
-
-# Installs mysql:
-echo "    Installing mariadb..."
-pkg install mariadb -y
-
-# Mounts storage and access to downloads dir:
-echo "    Mounting Termux storage system..."
-termux-setup-storage
-
-# Installs screenfetch for tunning the enviroment:
-echo "    Installing screenfetch..."
-pkg install screenfetch -y
-
-# Installs htop for control how much RAM are we consuming:
-echo "    Installing htop..."
-pkg install htop -y
 
 # WARNING: ROOT ZONE!!!
 # Asks for root and tries to setup your enviroment according to this:
@@ -64,7 +12,68 @@ then
 	pkg install root-repo -y
 	pkg install tsu -y
         echo "    You have SUPER USER PERMISIONS NOW! USE IT WISELY!!!"
+	echo "    If you have installed Kali NetHunter, want to use it instead Termux? [Y|n]"
+	read use_kali
+	if [ $use_kali != "n" ] && [ $use_kali != "N" ]
+	then
+		sudo cp -f ./.zshrc /data/local/nhsystem/kali-arm64/root
+		if [ ! -f ~/.bashrc ]
+		then
+		touch ~/.bashrc
+	fi
+	echo "sudo /data/data/com.offsec.nethunter/files/scripts/bootkali" >> ~/.bashrc
 fi
+	# Setups the nodejs/expo/git basic enviroment:
+	echo "    Insatalling git, curl, wget, sqlite and nodejs..."
+	pkg install git curl wget sqlite nodejs-lts -y
+	# Upgrades npm:
+	echo "    Installing npm..."
+	curl -qL https://www.npmjs.com/install.sh | sh
+	# Installs Expo and yarn
+	echo "    Installing Expo and yarn..."
+	npm i -g expo-cli eas-cli nodemon yarn -y
+
+	# Installs tmux (optional):
+	echo "    Installing tmux..."
+	pkg install tmux -y
+	
+	# Replaces config files into your root folder:
+	echo "    Replacing config files for bash and nano..."
+	cp -f ./.bashrc ~
+	cp -f ./.nanorc ~
+
+	# Clones the syntax highlighting config files from @scopatz
+	echo "    Cloning syntax highlighting config files and creating nano backup dir..."
+	cd ~
+	if [ ! -d .nano ]
+	then
+		mkdir .nano
+	fi
+	cd .nano
+	if [ ! -d backups ]
+	then
+		mkdir backups
+	fi
+	if [ ! -d nanorc ]
+	then
+		git clone https://github.com/scopatz/nanorc
+	fi
+
+	# Installs mysql:
+	echo "    Installing mariadb..."
+	pkg install mariadb -y
+
+	# Mounts storage and access to downloads dir:
+	echo "    Mounting Termux storage system..."
+	termux-setup-storage
+
+	# Installs screenfetch for tunning the enviroment:
+	echo "    Installing screenfetch..."
+	pkg install screenfetch -y
+
+	# Installs htop for control how much RAM are we consuming:
+	echo "    Installing htop..."
+	pkg install htop -y
 
 # Exit:
 echo "    See the Termux Wiki for more info."
