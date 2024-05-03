5459c4d8fe9458d4466b5aca6a4ae17f6d133017
Jakku Night <J97k107u45k117n@gmail.com>
2024-05-03 09:10:30 -0400

    Add support for syntax highlighting and autocompletion.

---
+++ target-install/release/deps/rfetch-b26bff32a6a498be.d
@@ -0,0 +1,11 @@
+/mss/target-install/release/deps/rfetch-b26bff32a6a498be: /root/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rfetch-0.4.2/src/main.rs /root/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rfetch-0.4.2/src/ecosys.rs /root/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rfetch-0.4.2/src/uname.rs
+
+/mss/target-install/release/deps/rfetch-b26bff32a6a498be.d: /root/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rfetch-0.4.2/src/main.rs /root/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rfetch-0.4.2/src/ecosys.rs /root/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rfetch-0.4.2/src/uname.rs
+
+/root/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rfetch-0.4.2/src/main.rs:
+/root/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rfetch-0.4.2/src/ecosys.rs:
+/root/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rfetch-0.4.2/src/uname.rs:
+
+# env-dep:CARGO_PKG_AUTHORS=Avery Murray <averylapine@gmail.com>
+# env-dep:CARGO_PKG_NAME=rfetch
+# env-dep:CARGO_PKG_VERSION=0.4.2
