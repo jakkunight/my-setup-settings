3cc2db96adbfd43977f4ed9ab2c61f8099fe58ae
Jakku Night <J97k107u45k117n@gmail.com>
2024-06-07 11:33:03 -0400

    Update NvChad configs.

--- dotfiles/nvim/lua/configs/lspconfig.lua
+++ dotfiles/nvim/lua/configs/lspconfig.lua
@@ -1,11 +1,43 @@
--- EXAMPLE 
+-- EXAMPLE
 local on_attach = require("nvchad.configs.lspconfig").on_attach
 local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "html", "cssls" }
-
+local servers = {
+  "html",
+  "cssls",
+  "astro",
+  "arduino_language_server",
+  "bashls",
+  "tsserver",
+  "biome",
+  "cmake",
+  "clangd",
+  "csharp_ls",
+  "css_variables",
+  "cssmodules_ls",
+  "dockerls",
+  "emmet_ls",
+  "htmx",
+  "hyprls",
+  "java_language_server",
+  "kotlin_language_server",
+  "ltex",
+  "texlab",
+  "markdown_oxide",
+  "nixd",
+  "nushell",
+  "powershell_es",
+  "pylyzer",
+  "rust_ananlyzer",
+  "sqlls",
+  "svelte",
+  "tailwindcss",
+  "prismals",
+  "yamlls",
+  "zls",
+}
 -- lsps with default config
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
